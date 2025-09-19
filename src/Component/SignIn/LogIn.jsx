b507f315c5a47d54865bbdc929fa854191ef7641
OziasMumbongo <oziasmumbongo67@gmail.com>
2025-09-19 09:59:13 +0200

    Fixed

--- src/Component/SignIn/LogIn.jsx
+++ src/Component/SignIn/LogIn.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './LogIn.css';
+import './Login.css';
 
 const LogIn = ({ setIsLoggedIn, setCart }) => {
   const [formData, setFormData] = useState({ name: '', email: '', password: '' });
