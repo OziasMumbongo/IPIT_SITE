3273af5f8704ab54f41cd2c14003cc322e01abe9
Ozias <oziasmumbongo67@gmail.com>
2025-11-03 12:48:42 +0200

    number 23

--- src/Component/SignUp/SignUp.jsx
+++ src/Component/SignUp/SignUp.jsx
@@ -73,7 +73,7 @@ const SignupForm = ()=> {
         </div>
 
         <div className="form-container">
-          <h2>Newcomer?2335465</h2>
+          <h2>Newcomer?23</h2>
           <p className="subtext">Register with ease</p>
 
           <form onSubmit={handleSubmit}>
